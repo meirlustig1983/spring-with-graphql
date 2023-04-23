b05f47f7887c64e3bd0c0879faff25ea2103c608
meirlustig <meirlustig1983@gmail.com>
2023-04-22 18:45:22 -0700

    SWG-002 Change 'Customer.java' to use lombok library, and clean

--- src/main/java/com/ml/springwithgraphql/SpringWithGraphqlApplication.java
+++ src/main/java/com/ml/springwithgraphql/SpringWithGraphqlApplication.java
@@ -9,5 +9,4 @@ public class SpringWithGraphqlApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringWithGraphqlApplication.class, args);
     }
-
 }
--- src/main/java/com/ml/springwithgraphql/data/Customer.java
+++ src/main/java/com/ml/springwithgraphql/data/Customer.java
@@ -6,100 +6,30 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Data;
 
+@Data
 @Entity
-@Table(name="CUSTOMERS")
+@Table(name = "CUSTOMERS")
 public class Customer {
     @Id
-    @Column(name="CUSTOMER_ID")
+    @Column(name = "CUSTOMER_ID")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-    @Column(name="FIRST_NAME")
+    @Column(name = "FIRST_NAME")
     private String firstName;
-    @Column(name="LAST_NAME")
+    @Column(name = "LAST_NAME")
     private String lastName;
-    @Column(name="EMAIL")
+    @Column(name = "EMAIL")
     private String email;
-    @Column(name="PHONE")
+    @Column(name = "PHONE")
     private String phoneNumber;
-    @Column(name="ADDRESS")
+    @Column(name = "ADDRESS")
     private String address;
-    @Column(name="CITY")
+    @Column(name = "CITY")
     private String city;
-    @Column(name="STATE")
+    @Column(name = "STATE")
     private String state;
-    @Column(name="ZIPCODE")
+    @Column(name = "ZIPCODE")
     private String zipCode;
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    public String getZipCode() {
-        return zipCode;
-    }
-
-    public void setZipCode(String zipCode) {
-        this.zipCode = zipCode;
-    }
 }
